29001e4caf61d6d94fad2ea0c18cc3bf34d28a86
Alexandre Steffen <steffen.alexandre@cobalt-team.ch>
2023-02-23 09:00:44 +0100

    Solved Issue #40

--- ASN/Q3/ex3.cpp
+++ ASN/Q3/ex3.cpp
@@ -13,7 +13,7 @@ int main()
 	Select_Var_Flottant(&var_f_1);
 	Select_Var_Flottant(&var_f_2);
 
-	std::cout << "Soustraction mode affichage flottant : X - Y = " << var_f_1 << " - " << var_f_2 << " = " << (var_f_1 - var_f_2) << std::endl;
+	std::cout << "Soustraction mode affichage flottant : X - Y = " << cout.precision(3) << var_f_1 << " - " << var_f_2 << " = " << (var_f_1 - var_f_2) << std::endl;
 }
 
 void Select_Var_Flottant(float* valRetour)
