bbc17f45e19df8a56b6e0bdbc1ceb08d61f48e2c
Alexandre Steffen <steffen.alexandre@cobalt-team.ch>
2023-02-23 09:26:40 +0100

    Q3 solved
    Finished last corrections during the presentation of the solutions

--- ASN/Q3/ex3.cpp
+++ ASN/Q3/ex3.cpp
@@ -3,21 +3,21 @@
 #include <iomanip>
 
 // Prototype Declaration //
-void Select_Var_Flottant(float* valRetour);
+void Select_Var_Flottant(float &valRetour);
 
 int main()
 {
 	float var_f_1;
 	float var_f_2;
 
-	Select_Var_Flottant(&var_f_1);
-	Select_Var_Flottant(&var_f_2);
+	Select_Var_Flottant(var_f_1);
+	Select_Var_Flottant(var_f_2);
 
 	std::cout << setiosflags(std::ios::scientific);
 	std::cout << "Soustraction mode affichage flottant : X - Y = " << std::cout.precision(3) << var_f_1 << " - " << var_f_2 << " = " << (var_f_1 - var_f_2) << std::endl;
 }
 
-void Select_Var_Flottant(float* valRetour)
+void Select_Var_Flottant(float &valRetour)
 {
-	std::cin >> *valRetour;
+	std::cin >> valRetour;
 }
