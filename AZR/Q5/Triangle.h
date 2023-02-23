fb1afc2a45a1c3e8b0311b9ccab17418009af8bd
Ali-Z0 <107996590+Ali-Z0@users.noreply.github.com>
2023-02-23 10:44:44 +0100

    Correction question 5

--- AZR/Q5/Triangle.h
+++ AZR/Q5/Triangle.h
@@ -1,6 +1,7 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include "Signal.h"
 
 /**
   * class Triangle
--- AZR/Q5/Utilisateur.h
+++ AZR/Q5/Utilisateur.h
@@ -1,8 +1,6 @@
 #ifndef USER_H
 #define USER_H
 
-#include"GenerateurSignal.h"
-
 /**
   * class Utilisateur
   *
