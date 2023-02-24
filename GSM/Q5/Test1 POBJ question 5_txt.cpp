cf16665626e33998d8225d154f77ccbd78e5145f
Gajamukan <118287982+Gajamukan@users.noreply.github.com>
2023-02-24 16:52:15 +0100

    Update Test1 POBJ question 5_txt.cpp
    Correction code

--- GSM/Q5/Test1 POBJ question 5_txt.cpp
+++ GSM/Q5/Test1 POBJ question 5_txt.cpp
@@ -4,11 +4,17 @@ Question 5
 #ifndef Utilisateur_h
 #define Utilisateur_h
 
+class GenerateurSignal;
+
 class Utilisateur {
 
 		public:
 			void ConfigurerGenerateur();
 			void ObserverSignal ();
+
+		public : 
+			GenerateurSignal *myGenerateurSignal;
+
 		};
 #endif //Utilisateur_h
 
@@ -17,16 +23,21 @@ class Utilisateur {
 #ifndef GenerateurSignal_h
 #define GenerateurSignal_h
 
+#include "Signal.h"
+
 class GenerateurSignal {
 
 		public:
 			int ChoisirSignal();
 			void ChoisirFrequence ();
 			void ChoisirAmplitude ();
+			GenerateurSignal();
+			~GenerateurSignal();
 
 		private:
 			int m_Frequence;
 			int m_Amplitude;
+			Signal mySignal;
 		
 		};
 
@@ -37,42 +48,33 @@ class GenerateurSignal {
 #define Signal_h
 
 class Signal {
+		public:
+			virtual void CalculerEchantillons(int Amplitude, int DutyCycle);
+			void AfficherEchantillons ();
+
+		private:
+			void GetPeriodeEch ();
+
 
 		protected:
-
-
-		protected:
-			int m_Frequence;
-
-		
+			  int m_TableEch[20];
+			  int m_PeriodeEchantillonage;	
 		};
-
 #endif //Signal_h
 
 
 #ifndef Triangle_h
 #define Triangle_h
 
-class Triangle {
+#include Signal.h
+
+class Triangle : public Signal
+
+		{
 
 		public:
 			void CalculerEchantillons(int Amplitude , int DutyCycle)
 
-
-
-		
 		};
 
 #endif //Triangle_h
-
-
-
-
-
-
-
-
-
-
-
-
